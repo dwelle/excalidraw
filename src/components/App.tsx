ffaa25cf9d891dd4009bd805557c3551708fa565
dwelle <luzar.david@gmail.com>
2021-03-05 21:58:40 +0100

    make collaborators vertical & support custom topRight UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -451,6 +451,7 @@ class App extends React.Component<AppProps, AppState> {
     const { zenModeEnabled, viewModeEnabled } = this.state;
 
     const {
+      renderTopRight,
       onCollabButtonClick,
       onExportToBackend,
       renderFooter,
@@ -477,6 +478,7 @@ class App extends React.Component<AppProps, AppState> {
           <IsMobileContext.Provider value={this.isMobile}>
             <LayerUI
               onHomeButtonClick={this.props.onHomeButtonClick}
+              renderTopRight={renderTopRight}
               canvas={this.canvas}
               appState={this.state}
               setAppState={this.setAppState}
