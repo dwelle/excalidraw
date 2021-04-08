53c719c64317ceb464b882eddf24ff372744ded5
dwelle <luzar.david@gmail.com>
2021-04-08 23:33:33 +0200

    break to mobile at 800px

--- src/constants.ts
+++ src/constants.ts
@@ -155,7 +155,7 @@ export const DEFAULT_UI_OPTIONS: AppProps["UIOptions"] = {
   },
 };
 
-export const MQ_MAX_WIDTH_PORTRAIT = 730;
+export const MQ_MAX_WIDTH_PORTRAIT = 800;
 export const MQ_MAX_WIDTH_LANDSCAPE = 1000;
 export const MQ_MAX_HEIGHT_LANDSCAPE = 500;
 
