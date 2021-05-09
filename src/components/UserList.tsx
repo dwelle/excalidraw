4e92c7b66c83ef1e80747350c7db67a066a60501
dwelle <luzar.david@gmail.com>
2021-05-09 22:45:30 +0200

    make UserList vertical

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,16 +2,37 @@ import "./UserList.scss";
 
 import React from "react";
 import clsx from "clsx";
+import { AppState } from "../types";
 
 type UserListProps = {
   children: React.ReactNode;
   className?: string;
   mobile?: boolean;
+  collaborators: AppState["collaborators"];
+  layout: "vertical" | "horizontal";
 };
 
-export const UserList = ({ children, className, mobile }: UserListProps) => {
+export const UserList = ({
+  children,
+  className,
+  mobile,
+  collaborators = new Map(),
+  layout,
+}: UserListProps) => {
+  const threshold = layout === "vertical" ? 6 : 3;
+  const offset =
+    collaborators.size > threshold
+      ? Math.min(collaborators.size - threshold, 15) * -2
+      : 4;
   return (
-    <div className={clsx("UserList", className, { UserList_mobile: mobile })}>
+    <div
+      className={clsx(`UserList layout-${layout}`, className, {
+        UserList_mobile: mobile,
+      })}
+      style={{
+        ["--itemOffset" as any]: `${offset}px`,
+      }}
+    >
       {children}
     </div>
   );
