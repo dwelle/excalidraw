b5767187479b0ef07921e971cbf508ab91ce245d
dwelle <luzar.david@gmail.com>
2021-05-09 22:45:30 +0200

    make UserList vertical

--- src/components/LayerUI.tsx
+++ src/components/LayerUI.tsx
@@ -597,22 +597,28 @@ const LayerUI = ({
               },
             )}
           >
-            <UserList>
-              {appState.collaborators.size > 0 &&
-                Array.from(appState.collaborators)
-                  // Collaborator is either not initialized or is actually the current user.
-                  .filter(([_, client]) => Object.keys(client).length !== 0)
-                  .map(([clientId, client]) => (
-                    <Tooltip
-                      label={client.username || "Unknown user"}
-                      key={clientId}
-                    >
-                      {actionManager.renderAction("goToCollaborator", clientId)}
-                    </Tooltip>
-                  ))}
-            </UserList>
             {renderTopRight?.(isMobile, appState)}
           </div>
+          <UserList
+            className={clsx("zen-mode-transition", {
+              "transition-right": zenModeEnabled,
+            })}
+            layout="vertical"
+            collaborators={appState.collaborators}
+          >
+            {appState.collaborators.size > 0 &&
+              Array.from(appState.collaborators)
+                // Collaborator is either not initialized or is actually the current user.
+                .filter(([_, client]) => Object.keys(client).length !== 0)
+                .map(([clientId, client]) => (
+                  <Tooltip
+                    label={client.username || "Unknown user"}
+                    key={clientId}
+                  >
+                    {actionManager.renderAction("goToCollaborator", clientId)}
+                  </Tooltip>
+                ))}
+          </UserList>
         </div>
       </FixedSideContainer>
     );
