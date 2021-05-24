3fd995a3e29a6f9640ae1776fb182a6d3a69140b
dwelle <luzar.david@gmail.com>
2021-05-24 14:12:56 +0200

    fix uselist props & remove custom footer due to type issues

--- src/components/LayerUI.tsx
+++ src/components/LayerUI.tsx
@@ -379,22 +379,6 @@ const LayerUI = ({
               },
             )}
           >
-            <UserList>
-              {appState.collaborators.size > 0 &&
-                Array.from(appState.collaborators)
-                  // Collaborator is either not initialized or is actually the current user.
-                  .filter(([_, client]) => Object.keys(client).length !== 0)
-                  .map(([clientId, client]) => (
-                    <Tooltip
-                      label={client.username || "Unknown user"}
-                      key={clientId}
-                    >
-                      {actionManager.renderAction("goToCollaborator", {
-                        id: clientId,
-                      })}
-                    </Tooltip>
-                  ))}
-            </UserList>
             {renderTopRightUI?.(deviceType.isMobile, appState)}
           </div>
           <UserList
@@ -413,7 +397,9 @@ const LayerUI = ({
                     label={client.username || "Unknown user"}
                     key={clientId}
                   >
-                    {actionManager.renderAction("goToCollaborator", clientId)}
+                    {actionManager.renderAction("goToCollaborator", {
+                      id: clientId,
+                    })}
                   </Tooltip>
                 ))}
           </UserList>
