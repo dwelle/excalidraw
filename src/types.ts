b9f309c48aac848fd6db48ae0f4bf83a2e463881
dwelle <luzar.david@gmail.com>
2021-07-03 21:46:42 +0200

    canvasActions & related

--- src/types.ts
+++ src/types.ts
@@ -213,7 +213,7 @@ export type ExcalidrawAPIRefValue =
 type InitialData = ImportedDataState & { scrollX?: number; scrollY?: number };
 
 export interface ExcalidrawProps {
-  id: string | null;
+  id?: string | null;
   onChange?: (
     elements: readonly ExcalidrawElement[],
     appState: AppState,
@@ -297,10 +297,10 @@ export type ExportOpts = {
   ) => JSX.Element;
 };
 
-type CanvasActions = {
+export type CanvasActions = {
   changeViewBackgroundColor?: boolean;
   clearCanvas?: boolean;
-  export?: false | ExportOpts;
+  export?: false | ExportOpts | (() => void);
   loadScene?: boolean;
   saveToActiveFile?: boolean;
   theme?: boolean;
@@ -308,12 +308,12 @@ type CanvasActions = {
 };
 
 export type UIOptions = {
-  canvasActions?: CanvasActions;
+  canvasActions?: CanvasActions | false;
 };
 
 export type AppProps = ExcalidrawProps & {
   UIOptions: {
-    canvasActions: Required<CanvasActions> & { export: ExportOpts };
+    canvasActions: Required<CanvasActions> | false;
   };
   detectScroll: boolean;
   handleKeyboardGlobally: boolean;
