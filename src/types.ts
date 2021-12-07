060790b0776269c31d0b32de5c401ed7f512e70e
dwelle <luzar.david@gmail.com>
2021-12-07 14:04:06 +0100

    fix renderTopRightUI

--- src/types.ts
+++ src/types.ts
@@ -284,6 +284,7 @@ export interface ExcalidrawProps {
   renderTopRightUI?: (
     isMobile: boolean,
     appState: AppState,
+    canvas: HTMLCanvasElement | null,
   ) => JSX.Element | null;
   renderFooter?: (isMobile: boolean, appState: AppState) => JSX.Element;
   langCode?: Language["code"];
