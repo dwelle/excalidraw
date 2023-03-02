f19aadc1e9367b0ce301a725e25e7fcc8ea7e19a
dwelle <luzar.david@gmail.com>
2023-03-02 13:31:30 +0100

    custom
    export actions
    
    temp: force production build

--- packages/element/src/renderElement.ts
+++ packages/element/src/renderElement.ts
@@ -541,10 +541,15 @@ const drawElementOnCanvas = (
   }
 };
 
-export const elementWithCanvasCache = new WeakMap<
+export let elementWithCanvasCache = new WeakMap<
   ExcalidrawElement,
   ExcalidrawElementWithCanvas
 >();
+
+export const clearRenderCache = () => {
+  elementWithCanvasCache = new WeakMap();
+  pathsCache = new WeakMap();
+};
 
 const generateElementWithCanvas = (
   element: NonDeletedExcalidrawElement,
@@ -1025,7 +1030,7 @@ export const renderElement = (
   context.globalAlpha = 1;
 };
 
-export const pathsCache = new WeakMap<ExcalidrawFreeDrawElement, Path2D>([]);
+export let pathsCache = new WeakMap<ExcalidrawFreeDrawElement, Path2D>([]);
 
 export function generateFreeDrawShape(element: ExcalidrawFreeDrawElement) {
   const svgPathData = getFreeDrawSvgPath(element);
