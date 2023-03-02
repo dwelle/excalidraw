147fba665f527475aad088f3d1306b190a2fc192
dwelle <luzar.david@gmail.com>
2023-03-02 13:31:30 +0100

    custom

--- packages/excalidraw/components/LayerUI.tsx
+++ packages/excalidraw/components/LayerUI.tsx
@@ -82,6 +82,7 @@ interface LayerUIProps {
   UIOptions: AppProps["UIOptions"];
   onExportImage: AppClassProperties["onExportImage"];
   renderWelcomeScreen: boolean;
+  onHomeButtonClick: () => void;
   children?: React.ReactNode;
   app: AppClassProperties;
   isCollaborating: boolean;
@@ -140,6 +141,7 @@ const LayerUI = ({
   UIOptions,
   onExportImage,
   renderWelcomeScreen,
+  onHomeButtonClick,
   children,
   app,
   isCollaborating,
@@ -507,6 +509,7 @@ const LayerUI = ({
       {device.editor.isMobile && (
         <MobileMenu
           app={app}
+          onHomeButtonClick={onHomeButtonClick}
           appState={appState}
           elements={elements}
           actionManager={actionManager}
