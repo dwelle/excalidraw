5382b0acd96c1aada850c1e95922666d452c72cb
dwelle <luzar.david@gmail.com>
2023-10-06 00:00:25 +0200

    [pr] non-interactivity

--- packages/excalidraw/types.ts
+++ packages/excalidraw/types.ts
@@ -573,6 +573,8 @@ export interface ExcalidrawProps {
   ) => JSX.Element | null;
   aiEnabled?: boolean;
   showDeprecatedFonts?: boolean;
+  interactive?: boolean;
+  ui?: boolean;
 }
 
 export type SceneData = {
