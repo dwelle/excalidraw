71da0facc1bc87f8574fdb7bdd817baf0e2c6a1b
dwelle <luzar.david@gmail.com>
2023-10-10 16:13:37 +0200

    [pr] props.activeTool

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -3175,6 +3175,13 @@ class App extends React.Component<AppProps, AppState> {
     }
 
     if (
+      this.props.activeTool &&
+      this.props.activeTool.type !== this.state.activeTool.type
+    ) {
+      this.setActiveTool(this.props.activeTool);
+    }
+
+    if (
       prevState.zoom.value !== this.state.zoom.value ||
       prevState.scrollX !== this.state.scrollX ||
       prevState.scrollY !== this.state.scrollY
@@ -5258,14 +5265,7 @@ class App extends React.Component<AppProps, AppState> {
     }
 
     const nextActiveTool = updateActiveTool(this.state, tool);
-    if (nextActiveTool.type === "hand") {
-      setCursor(this.interactiveCanvas, CURSOR_TYPE.GRAB);
-    } else if (!isHoldingSpace) {
-      setCursorForShape(this.interactiveCanvas, {
-        ...this.state,
-        activeTool: nextActiveTool,
-      });
-    }
+
     if (isToolIcon(document.activeElement)) {
       this.focusContainer();
     }
@@ -5289,9 +5289,10 @@ class App extends React.Component<AppProps, AppState> {
       if (nextActiveTool.type === "freedraw") {
         this.store.scheduleCapture();
       }
+      let nextState: AppState;
 
       if (nextActiveTool.type === "lasso") {
-        return {
+        nextState = {
           ...prevState,
           ...commonResets,
           activeTool: nextActiveTool,
@@ -5305,7 +5306,7 @@ class App extends React.Component<AppProps, AppState> {
               }),
         };
       } else if (nextActiveTool.type !== "selection") {
-        return {
+        nextState = {
           ...prevState,
           ...commonResets,
           activeTool: nextActiveTool,
@@ -5314,12 +5315,14 @@ class App extends React.Component<AppProps, AppState> {
           editingGroupId: null,
           multiElement: null,
         };
+      } else {
+        nextState = {
+          ...prevState,
+          ...commonResets,
+          activeTool: nextActiveTool,
+        };
       }
-      return {
-        ...prevState,
-        ...commonResets,
-        activeTool: nextActiveTool,
-      };
+      return nextState;
     });
   };
 
