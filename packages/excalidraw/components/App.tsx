ea1aa82c065d26fd322f8d86274bd706abd550d9
dwelle <luzar.david@gmail.com>
2023-10-10 16:13:37 +0200

    [pr] props.activeTool

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -2698,6 +2698,13 @@ class App extends React.Component<AppProps, AppState> {
     }
 
     if (
+      this.props.activeTool &&
+      this.props.activeTool.type !== this.state.activeTool.type
+    ) {
+      this.setActiveTool(this.props.activeTool);
+    }
+
+    if (
       prevState.zoom.value !== this.state.zoom.value ||
       prevState.scrollX !== this.state.scrollX ||
       prevState.scrollY !== this.state.scrollY
@@ -4567,11 +4574,7 @@ class App extends React.Component<AppProps, AppState> {
     }
 
     const nextActiveTool = updateActiveTool(this.state, tool);
-    if (nextActiveTool.type === "hand") {
-      setCursor(this.interactiveCanvas, CURSOR_TYPE.GRAB);
-    } else if (!isHoldingSpace) {
-      setCursorForShape(this.interactiveCanvas, this.state);
-    }
+
     if (isToolIcon(document.activeElement)) {
       this.focusContainer();
     }
@@ -4595,9 +4598,10 @@ class App extends React.Component<AppProps, AppState> {
       if (nextActiveTool.type === "freedraw") {
         this.store.shouldCaptureIncrement();
       }
+      let nextState: AppState;
 
       if (nextActiveTool.type !== "selection") {
-        return {
+        nextState = {
           ...prevState,
           activeTool: nextActiveTool,
           selectedElementIds: makeNextSelectedElementIds({}, prevState),
@@ -4606,12 +4610,21 @@ class App extends React.Component<AppProps, AppState> {
           multiElement: null,
           ...commonResets,
         };
+      } else {
+        nextState = {
+          ...prevState,
+          activeTool: nextActiveTool,
+          ...commonResets,
+        };
       }
-      return {
-        ...prevState,
-        activeTool: nextActiveTool,
-        ...commonResets,
-      };
+
+      if (nextActiveTool.type === "hand") {
+        setCursor(this.interactiveCanvas, CURSOR_TYPE.GRAB);
+      } else if (!isHoldingSpace) {
+        setCursorForShape(this.interactiveCanvas, nextState);
+      }
+
+      return nextState;
     });
   };
 
