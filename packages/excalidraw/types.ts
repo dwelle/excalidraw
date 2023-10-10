678b1e08b94f4fdb5ae44138a876b5552a12dc15
dwelle <luzar.david@gmail.com>
2023-10-10 16:13:37 +0200

    [pr] props.activeTool

--- packages/excalidraw/types.ts
+++ packages/excalidraw/types.ts
@@ -650,6 +650,14 @@ export interface ExcalidrawProps {
   renderScrollbars?: boolean;
   interactive?: boolean;
   ui?: boolean;
+  activeTool?:
+    | {
+        type: ToolType;
+      }
+    | {
+        type: "custom";
+        customType: string;
+      };
 }
 
 export type SceneData = {
