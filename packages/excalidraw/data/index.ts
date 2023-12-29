55f8b13da823743790b900b446efd97cac1d2af1
dwelle <5153846+dwelle@users.noreply.github.com>
2023-12-29 12:34:43 +0100

    [pr] esm

--- packages/excalidraw/data/index.ts
+++ packages/excalidraw/data/index.ts
@@ -153,7 +153,7 @@ export const exportAsImage = async (
     let blob = await canvasToBlob(tempCanvas);
     if (appState.exportEmbedScene) {
       blob = await (
-        await import(/* webpackChunkName: "image" */ "./image")
+        await import("./image")
       ).encodePngMetadata({
         blob,
         metadata: serializeAsJSON(elements, appState, files, "local"),
