ba13e6bc69e7fed68dc1c505a2db987d39cabc61
dwelle <5153846+dwelle@users.noreply.github.com>
2024-03-08 10:09:25 +0100

    [pr] export rewrite

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -109,6 +109,7 @@ import {
   loadDesktopUIModePreference,
   setDesktopUIMode,
   isSelectionLikeTool,
+  isProdEnv,
 } from "@excalidraw/common";
 
 import {
@@ -208,7 +209,11 @@ import {
   getApproxMinLineHeight,
   getMinTextElementWidth,
   ShapeCache,
+  clearRenderCache,
   getRenderOpacity,
+} from "@excalidraw/element";
+
+import {
   editGroupForSelectedElement,
   getElementsInGroup,
   getSelectedGroupIdForElement,
@@ -343,8 +348,7 @@ import {
   parseDataTransferEvent,
   type ParsedDataTransferFile,
 } from "../clipboard";
-
-import { exportCanvas, loadFromBlob } from "../data";
+import { exportAsImage, loadFromBlob } from "../data";
 import Library, { distributeLibraryItemsOnSquareGrid } from "../data/library";
 import { restoreAppState, restoreElements } from "../data/restore";
 import { getCenter, getDistance } from "../gesture";
@@ -2240,18 +2244,20 @@ class App extends React.Component<AppProps, AppState> {
     opts: { exportingFrame: ExcalidrawFrameLikeElement | null },
   ) => {
     trackEvent("export", type, "ui");
-    const fileHandle = await exportCanvas(
+    const fileHandle = await exportAsImage({
       type,
-      elements,
-      this.state,
-      this.files,
-      {
+      data: {
+        elements,
+        appState: this.state,
+        files: this.files,
+      },
+      config: {
         exportBackground: this.state.exportBackground,
         name: this.getName(),
         viewBackgroundColor: this.state.viewBackgroundColor,
         exportingFrame: opts.exportingFrame,
       },
-    )
+    })
       .catch(muteFSAbortError)
       .catch((error) => {
         console.error(error);
@@ -2974,8 +2980,8 @@ class App extends React.Component<AppProps, AppState> {
 
     this.onChangeEmitter.clear();
 
-    if (import.meta.env.mode === ENV.PRODUCTION) {
-      this.history = new History();
+    if (isProdEnv()) {
+      this.history = new History(this.store);
       this.library = new Library(this);
       this.actionManager = new ActionManager(
         this.syncActionResult,
