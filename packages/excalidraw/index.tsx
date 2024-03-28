680144d3c52bb078b8a7b50413c89ddb2ce8f739
dwelle <5153846+dwelle@users.noreply.github.com>
2024-03-28 17:33:37 +0100

    export CommandPalette

--- packages/excalidraw/index.tsx
+++ packages/excalidraw/index.tsx
@@ -323,3 +323,5 @@ export * as actions from "./actions/index";
 export { duplicateElements, duplicateElement } from "./element/newElement";
 
 export { parseMermaidToExcalidraw } from "@excalidraw/mermaid-to-excalidraw";
+
+export { CommandPalette } from "./components/CommandPalette/CommandPalette";
