14cc2801acdcdb31130130e0ff090149db7edc30
dwelle <5153846+dwelle@users.noreply.github.com>
2025-03-25 11:35:06 +0100

    stuff

--- packages/excalidraw/types.ts
+++ packages/excalidraw/types.ts
@@ -539,6 +539,7 @@ export type OnUserFollowedPayload = {
 
 export interface ExcalidrawProps {
   id?: string | null;
+  className?: string;
   onChange?: (
     elements: readonly OrderedExcalidrawElement[],
     appState: AppState,
