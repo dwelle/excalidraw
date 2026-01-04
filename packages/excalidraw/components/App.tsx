d425ff4481d98fa5eb5421e217cd39dd197c49a8
dwelle <5153846+dwelle@users.noreply.github.com>
2026-01-04 19:26:09 +0100

    wip

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -3138,7 +3138,6 @@ class App extends React.Component<AppProps, AppState> {
       this.rc = null;
       // @ts-ignore
       this.excalidrawContainerRef.current = undefined;
-      this.nearestScrollableContainer = undefined;
       this.excalidrawContainerValue = { container: null, id: "unmounted" };
     }
 
