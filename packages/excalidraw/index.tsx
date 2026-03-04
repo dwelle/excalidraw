63da605c9c3e53737dd38570ba8e744d474c3e4b
dwelle <5153846+dwelle@users.noreply.github.com>
2026-03-04 16:47:17 +0100

    fixes

--- packages/excalidraw/index.tsx
+++ packages/excalidraw/index.tsx
@@ -434,7 +434,7 @@ export {
   renderSpreadsheet,
   tryParseSpreadsheet,
   isSpreadsheetValidForChartType,
-} from "./charts";
+} from "./charts/index";
 
 // -----------------------------------------------------------------------------
 // useExcalidrawStateValue() wrapper for host apps for the return type to reflect the
@@ -484,5 +484,3 @@ export * as actions from "./actions/index";
 export { duplicateElements, duplicateElement } from "../element/src/duplicate";
 
 export { parseMermaidToExcalidraw } from "@excalidraw/mermaid-to-excalidraw";
-
-export { CommandPalette } from "./components/CommandPalette/CommandPalette";
